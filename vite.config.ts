761bfb5318ae3bee9f5d00d6e942ca06b9100ec9
Jack Jiang <578091306@qq.com>
2025-11-13 20:00:33 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(config => {
+  const envDir = "./env";
+  const envPrefix = ["ENV"];
+
   return {
+    envDir,
+    envPrefix,
+    server: {
+      host: '0.0.0.0',
+      allowedHosts: true
+    },
     build: {
       rollupOptions: {
         output: {
